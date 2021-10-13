6ecef77b3adf9593a214682dc8e00a0e64297d73
dongyueqian <874974405@qq.com>
2021-10-13 21:05:20 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
 import pytest
 if __name__ == '__main__':
-    command_line = ['-s', '-v', 'testcases', '--alluredir=reports/json']
+    command_line = ['-s', '-v', 'testcases', '--alluredir=/reports/json']
     pytest.main(command_line)
