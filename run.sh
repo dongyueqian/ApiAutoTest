34c474ef69dbfb63b09558e9ef60918b31e6ac6c
dongyueqian <874974405@qq.com>
2021-10-13 19:49:00 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #pytest testcases/test_unnormal.py
 #pytest testcases --html=./reports/report.html --self-contained-html
-pytest --alluredir ./reports/json
+#pytest --alluredir ./reports/json
+main.py
 allure generate reports/json -o reports/allure_result/
