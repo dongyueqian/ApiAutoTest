bdc43b0bcbd320a4700a2300177407ff911ebca1
dongyueqian <874974405@qq.com>
2021-10-13 17:25:39 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #pytest testcases/test_unnormal.py
-#pytest testcases --html=./reports/report.html --self-contained-html
-pytest testcases --alluredir ./reports
+pytest testcases --html=./reports/report.html --self-contained-html
+# pytest testcases --alluredir ./reports
 # allure serve reports  #生成allure报告
