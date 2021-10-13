7170cef30eafed3538ef072b2099dc6de57412d5
dongyueqian <874974405@qq.com>
2021-10-13 18:29:13 +0800

    allure报告

--- run.sh
+++ run.sh
@@ -1,2 +1,4 @@
 #pytest testcases/test_unnormal.py
-pytest testcases --html=./reports/report.html --self-contained-html
+#pytest testcases --html=./reports/report.html --self-contained-html
+pytest --alluredir ./reports/json
+allure generate reports/json -o reports/allure_result/
